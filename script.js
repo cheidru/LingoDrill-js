19e77752b4620c94d60f4785406b8039d6c4efdf
Igor Checheurov <chei@yandex.ru>
2023-01-23 00:04:37 +0300

    feat: add list of files from DB

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-let addFileDialogue = document.querySelector("#add-file-dialogue");
-let fileOpen = addFileDialogue.addEventListener('change', function() {
-        let file = this.files[0];
-        console.log(file.name);
+// ToDo:
+// Generate list of files in DB
 
+let dbExist = true;
 
+function listFilesFromDB() {        
+        // Try to open DB
+        let openDB = indexedDB.open("audioBase", 1);
 
+        openDB.onsuccess = function() {
+                let db = openDB.result;
+                let readFliesTransaction = db.transaction('audio', 'readonly');
+                let audioIndex = readFliesTransaction.index("date");
+                let aList = audioIndex.getAll();
 
-})
+                // Read audio file names from DB object to li elements
+                for(let aObject of aList) {
+                        let newLi = document.createElement("li");
+                        newLi.textContent = aObject.aName;
+                        newLi.dataset.audioIndex = aObject.id;
+                        ul.append(newLi);
+                }
+        };
+
+        openDB.onerror = console.error; // Show error message        
+        openDB.onupgradeneeded = () => dbExist = false;
+}
 
 
 // Function to open or create DB and return it to callback function
 function withDB (callback) {
-        // Try to open DB
-        let openDB = indexedDB.open("audioBase", 1);
-
-        openDB.onsuccess = function(e) {
-                let db = openDB.result;
-                // Start callback function on the db
-                callback(db);
-        };
-
-        openDB.onerror = console.error; // Show error message
-        
         openDB.onupgradeneeded = function(e) {
                 switch(e.oldVersion) {
                         case 0: // DB doesn't exist. Create DB structure
@@ -29,16 +36,31 @@ function withDB (callback) {
                                 let rangesStore = db.createObjectStore('ranges', {keyPath: 'id'}, {autoIncrement: 'true'});
                                 let subtitlesStore = db.createObjectStore('subtitles', {keyPath: 'id'}, {autoIncrement: 'true'});
                 };
-                rangesStore.createIndex("audioID", "prio");
-                subtitlesStore.createIndex("audioID", "rangeID");
+                // Create additional indexes to stores
 
-                // ToDo:
-                // Fetch file and metadata from input into blob
-                // Create transaction
-                // Get data into DB
-};
+                // Index on file addition date to range the list of files
+                audioStore.createIndex('dateIndex', 'date', {unique: false});
+                // Index to link to audio ID in audioStore
+                rangesStore.createIndex('audioIndex', 'audio', {unique: false});
+                // Index to show a range priority
+                rangesStore.createIndex('rangePrioIndex', 'prio', {unique: false});
+                // Index to link subtitle fragment to range ID in rangeStore
+                subtitlesStore.createIndex("rangeIndex", "rangeID", {unique: true});
+        };
+    
+        // ToDo:
+        // Fetch file and metadata from input into blob
+        // Create transaction
+        // Get data into DB
 }
 
+
+// Select an audio file from local file system
+let addFileDialogue = document.querySelector("#add-file-dialogue");
+let fileOpen = addFileDialogue.addEventListener('change', function() {
+        let file = this.files[0];
+        console.log(file.name);
+})
+
 // ToDo:
-// Generatye list of files in DB
 // Handle opening of the selected file from the list
