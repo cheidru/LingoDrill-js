f9f1b5b676f2861c06d2fe00333f4e0228dbe375
Igor Checheurov <chei@yandex.ru>
2026-03-11 00:25:20 +0300

    Updates

--- LingoDrill-js/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  // base: '/',
-  base: '/LingoDrill-js/LingoDrill-js',
-  plugins: [react()],
-})
